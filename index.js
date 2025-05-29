ad929edf3ab0294e6cb0bb2db2138b41901e067a
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-05-29 19:52:19 +0600

    added applient

--- index.js
+++ index.js
@@ -47,6 +47,17 @@ async function run() {
     });
 
     // Job application related API's
+    app.get('/applications', async(req, res) =>{
+        const email = req.query.email;
+
+        const query = {
+            applicant : email
+        };
+        const result = await applicationsCollection.find(query).toArray();
+        res.send(result);
+    })
+
+
     app.post('/applications', async(req, res) =>{
         const application = req.body;
         console.log(application);
