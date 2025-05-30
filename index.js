1d2af8c0aa0fbfa7bf7440dda3a898592c50f329
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-05-30 21:05:31 +0600

    added job list

--- index.js
+++ index.js
@@ -27,22 +27,45 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
 
     const jobsCollection = client.db('career-code').collection('jobs');
     const applicationsCollection = client.db('career-code').collection('applications')
 
     // Jobs API
     app.get('/jobs', async(req,res) =>{
-        const cursor = jobsCollection.find();
+
+
+        const email = req.query.email;
+        const query = {};
+        if(email){
+            query.hr_email = email;
+        }
+
+        const cursor = jobsCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
     });
+
+    // Could be done but should not by done
+    // app.get('/jobsByEmailAddress', async(req, res) =>{
+    //     const email = req.query.email;
+    //     const query = {hr_email: email};
+    //     const result = await jobsCollection.find(query).toArray();
+    //     res.send(result);
+    // })
 
     app.get('/jobs/:id', async(req, res) =>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const result = await jobsCollection.findOne(query);
+        res.send(result);
+    });
+
+    app.post('/jobs', async(req, res) =>{
+        const newJob = req.body;
+        console.log(newJob);
+        const result = await jobsCollection.insertOne(newJob);
         res.send(result);
     });
 
